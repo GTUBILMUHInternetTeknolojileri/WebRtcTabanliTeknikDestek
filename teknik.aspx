bddee6e178af903030b09fdf67db48bcbbd89e40
Ahmet DURNA <ahmet.durna@gmail.com>
2018-01-05 03:28:23 +0300

    Add files via upload

---
+++ teknik.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/teknik.Master" AutoEventWireup="true" CodeBehind="teknik.aspx.cs" Inherits="TeknikOfis.WebForm1" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+<%@ MasterType VirtualPath="~/teknik.Master" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
---
+++ teknik.aspx.designer.cs
@@ -0,0 +1,27 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace TeknikOfis {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// Master özelliği.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş özellik.
+        /// </remarks>
+        public new TeknikOfis.teknik Master {
+            get {
+                return ((TeknikOfis.teknik)(base.Master));
+            }
+        }
+    }
+}
